608bad9af18ad889f35c29dbbe7359b0281af3c8
Kumar Bishwajeet <bishwajeetk23@gmail.com>
2025-01-11 04:04:07 +0530

    Routes created

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 const port : string = process.env.PORT || '3000';
+app.get('/',(req,res)=>{
+    res.send("Server is running");
+})
+app.get('/signin',()=>{});
+app.get('/signup',()=>{});
+
+
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}/`);
     
